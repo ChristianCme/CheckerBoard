ce7f4b9a30f27a32df8cb4e5b7bb731d0c1fc97b
ChristianCme <cmehil.warn@gmail.com>
2018-01-26 18:18:32 -0600

    Add refresh statements and VBox

--- src/checkerboard/CheckerboardFXMLController.java
+++ src/checkerboard/CheckerboardFXMLController.java
@@ -13,6 +13,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -22,54 +24,57 @@ import javafx.stage.Stage;
 public class CheckerboardFXMLController implements Initializable,Startable
 {
     public Stage stage;
+    public CheckerBoard checkerBoard;
+    
+    @FXML
+    public VBox vbox;
     
     public ChangeListener<Number> lambdaChangeListener = (ObservableValue<? extends Number> observable, Number oldValue, final Number newValue) -> 
     {
-        
+        refresh(new CheckerBoard(this.checkerBoard.getNumRows(), this.checkerBoard.getNumCols(), 0, 0, this.checkerBoard.getLightColor(), this.checkerBoard.getDarkColor()));
     };
 
     
     @FXML
     public void handle3x3()
     {
-//        refresh(new CheckerBoard(3, 3, board.getHeight(), board.getWidth()).build());
+        refresh(new CheckerBoard(3, 3, this.checkerBoard.getHeight(), this.checkerBoard.getWidth(), this.checkerBoard.getLightColor(), this.checkerBoard.getDarkColor()));
     }
     
     @FXML
     public void handle8x8()
     {
-        
+        refresh(new CheckerBoard(8, 8, this.checkerBoard.getHeight(), this.checkerBoard.getWidth(), this.checkerBoard.getLightColor(), this.checkerBoard.getDarkColor()));
     }
     
     @FXML
     public void handle10x10()
     {
-        
+        refresh(new CheckerBoard(10, 10, this.checkerBoard.getHeight(), this.checkerBoard.getWidth(), this.checkerBoard.getLightColor(), this.checkerBoard.getDarkColor()));
     }
     
     @FXML
     public void handle16x16()
     {
-        
+        refresh(new CheckerBoard(16, 16, this.checkerBoard.getHeight(), this.checkerBoard.getWidth(), this.checkerBoard.getLightColor(), this.checkerBoard.getDarkColor()));
     }
     
     @FXML
     public void handleDefault()
     {
-        
+        refresh(new CheckerBoard(this.checkerBoard.getNumRows(), this.checkerBoard.getNumCols(), this.checkerBoard.getHeight(), this.checkerBoard.getWidth()));
     }
 
     @FXML
     public void handleBlue()
     {
-        
+        refresh(new CheckerBoard(this.checkerBoard.getNumRows(), this.checkerBoard.getNumCols(), this.checkerBoard.getHeight(), this.checkerBoard.getWidth(), Color.SKYBLUE, Color.DARKBLUE));
     }
     
     @Override
     public void start(Stage stage)
     {
         this.stage = stage;
-        
         this.stage.widthProperty().addListener(lambdaChangeListener);
         this.stage.heightProperty().addListener(lambdaChangeListener);
     }
@@ -80,9 +85,9 @@ public class CheckerboardFXMLController implements Initializable,Startable
         
     }    
     
-    public void refresh(AnchorPane anchorPane)
+    public void refresh(CheckerBoard checkerBoard)
     {
-  
+        checkerBoard.build();
     }
     
 }
